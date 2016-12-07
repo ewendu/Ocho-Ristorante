295f743e421c25432900ca3e8b1cd829bc06e4b7
jb3wa <jb@3wa.fr>
2016-12-07 20:05:59 +0000

    Template personalisation

--- application/controllers/HomeController.class.php
+++ application/controllers/HomeController.class.php
@@ -12,13 +12,14 @@ class HomeController
     	 */
     }
 
-    public function httpPostMethod(Http $http, array $formFields)
+    /*public function httpPostMethod(Http $http, array $formFields)
     {
     	/*
     	 * Méthode appelée en cas de requête HTTP POST
     	 *
     	 * L'argument $http est un objet permettant de faire des redirections etc.
     	 * L'argument $formFields contient l'équivalent de $_POST en PHP natif.
-    	 */
-    }
+    	
+    } */
+    
 }

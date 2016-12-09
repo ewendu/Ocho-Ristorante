b6678eb04e59d5d2a23268948bec35a656ff18e2
Dusch Ewen <dusch.ewen@gmail.com>
2016-12-09 13:43:23 +0000

    setting LoginView and created login model, login controller, adding form to loginview

---
+++ application/controllers/user/login/LoginController.class.php
@@ -0,0 +1,13 @@
+<?php
+
+class LoginController
+{
+    public function httpGetMethod()
+    {
+        // Method called when it's a get request 
+    }
+    public function httpPostMethod(Http $http, array $formFields)
+    {
+        
+    }
+}

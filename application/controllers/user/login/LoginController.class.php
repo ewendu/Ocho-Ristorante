355720ba06fcb4d9e2b7dc2ebf2673c6d8243f99
Dusch Ewen <dusch.ewen@gmail.com>
2016-12-12 09:25:50 +0000

    Fixed everything but a little bug on UserModel

---
+++ application/controllers/user/login/LoginController.class.php
@@ -0,0 +1,29 @@
+<?php
+    
+class LoginController
+{
+	public function httpGetMethod()
+	{
+	    return ['showonfail' => ''];
+	}
+	
+	public function httpPostMethod(Http $http, array $formFields)
+	{
+	    try
+		{
+    	    $userModel = new UserModel();
+    	    $user      = $userModel->findWithEmailPassword
+                (
+                    $formFields['email'],
+                    $formFields['password']
+                );
+                
+            return ['showonfail' => 'You successfully connected !'];
+		}
+		catch(Exception $e)
+		{
+		    return ['showonfail' => $e->getMessage()];
+		}
+	}
+	
+}

eaa8535f603796835f401c4edf502cdb1cc16e73
Dusch Ewen <dusch.ewen@gmail.com>
2016-12-15 12:18:08 +0000

    Starting to create  Basket controller

---
+++ application/controllers/basket/BasketController.class.php
@@ -0,0 +1,15 @@
+<?php
+
+
+
+class BasketSession
+{
+    public function httpPostMethod(Http $http, array $formFields)
+    {
+       var_dump($formFields);
+       
+       
+       
+        
+    }
+}
---
+++ application/www/js/classes/BasketSession.class.js
@@ -0,0 +1,40 @@
+'use strict';
+
+
+var BasketSession = function ()
+{
+    
+     this.items = new Array();
+    
+    
+};
+
+BasketSession.prototype.add = function(mealId, name, description, quantity, price) 
+{
+    
+    
+    mealId   = parseInt(mealId);
+    quantity = parseInt(quantity);
+    price    = parseFloat(price);
+    // Creating a new object with the parameters
+    var item = new Object();
+    item.mealId      = mealId;
+    item.Name        = name;
+    item.Description = description;
+    item.Quantity    = quantity;
+    item.Price       = price;
+    // if the product is already in the basket we just add the quantity
+    for(var i = 0; i < this.items.length; i++)
+    {
+        if(this.items[i].mealId == mealId)
+        {
+            this.items[i].Quantity += quantity;
+            return;
+        }
+    
+    }
+    // if not we add the product in the array
+    this.items.push(item);
+    
+    
+};
